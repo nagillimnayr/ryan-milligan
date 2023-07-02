2e74798bd572845ebb3c6010d80c1542795b882f
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-02 11:40:46 -0700

    build: install @tailwindcss/container-queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -72,5 +72,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography'), require("tailwindcss-animate")],
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/container-queries'), require("tailwindcss-animate")],
 }
