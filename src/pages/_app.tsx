1103ce381ad552c68329198159a20db8fccf4900
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-02 14:38:42 -0700

    feat: add ThemeProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import SiteLayout from '@/components/layout/site-layout/SiteLayout';
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SiteLayout>
+      <Component {...pageProps} />
+    </SiteLayout>
+  );
 }
