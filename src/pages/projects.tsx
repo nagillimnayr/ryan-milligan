b414919d89a537d305b47be23bda5a763fe92102
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-03 13:45:38 -0700

    feat: create projects page stub

---
+++ src/pages/projects.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import { type NextPage } from "next";
+
+const ProjectsPage: NextPage = () => {
+
+  return (
+    <>
+    <Head>
+      <title>Projects</title>
+      <meta name="description" content="" />
+      <link
+        rel="icon"
+        type="image/favicon"
+        href="/favicon.ico"
+        />
+    </Head>
+    <div>
+      <h1>Projects</h1>
+    </div>
+    </>
+  )
+}
+
+export default ProjectsPage;
