ab14363d76c79a7ce1070c0cef84793ca36a1b12
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-03 13:44:00 -0700

    feat: create about page stub

---
+++ src/pages/about.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import { type NextPage } from "next";
+
+const AboutPage: NextPage = () => {
+
+  return (
+    <>
+    <Head>
+      <title>About</title>
+      <meta name="description" content="" />
+      <link
+        rel="icon"
+        type="image/favicon"
+        href="/favicon.ico"
+        />
+    </Head>
+    <div>
+      <h1>About</h1>
+    </div>
+    </>
+  )
+}
+
+export default AboutPage;
