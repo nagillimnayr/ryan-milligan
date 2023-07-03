59581ae5786a1593e227b2ec1a8c088f0dbb1882
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-03 13:45:02 -0700

    feat: add link to header title

--- src/components/layout/site-layout/SiteHeader.tsx
+++ src/components/layout/site-layout/SiteHeader.tsx
@@ -1,14 +1,15 @@
+import Link from "next/link";
 
 
 export default function SiteHeader() {
   return(
       <header className="border-b-border border-b-2 h-20 flex flex-row items-center justify-start px-8 sticky top-0 z-50 bg-background">
-        <div className="flex flex-row w-fit h-fit items-center justify-start gap-4">
+        <Link href={'/'} className="flex flex-row w-fit h-fit items-center justify-start gap-4">
           <div className="border-2 border-border aspect-square w-10">
             
           </div>
           <h2 className="text-3xl font-extrabold font-orbitron">Site Header</h2>
-        </div>
+        </Link>
       </header>
   )
 }
