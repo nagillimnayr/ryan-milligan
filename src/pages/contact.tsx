0eda7d00a7cf2aaed8750febbefd36bcaf533a26
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-03 13:44:14 -0700

    feat: create contact page stub

---
+++ src/pages/contact.tsx
@@ -0,0 +1,29 @@
+import Head from 'next/head';
+import { type NextPage } from "next";
+
+const ContactPage: NextPage = () => {
+
+  return (
+    <>
+    <Head>
+      <title>Contact</title>
+      <meta name="description" content="" />
+      <link
+        rel="icon"
+        type="image/favicon"
+        href="/favicon/lightmode/favicon.svg"
+        />
+        <link
+          rel="icon"
+          type="favicon"
+          href="/favicon.ico"
+          />
+    </Head>
+    <div>
+      <h1>Contact</h1>
+    </div>
+    </>
+  )
+}
+
+export default ContactPage;
