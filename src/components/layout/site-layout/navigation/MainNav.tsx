e848a163715dcad7e4ca24bfc95290c43843f98a
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-04 00:32:59 -0700

    feat: add MainNav

---
+++ src/components/layout/site-layout/navigation/MainNav.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import NavButton from './NavButton';
+import { cn } from '@/lib/utils';
+
+const MainNav = () => {
+    return (
+        <div className={cn(`ml-auto flex h-full flex-row gap-0 font-display`)}>
+            <NavButton href="projects" className="px-5" />
+            <NavButton href="experiments" className="px-5" />
+            <NavButton href="contact" className="px-5" />
+        </div>
+    );
+};
+
+export default MainNav;
