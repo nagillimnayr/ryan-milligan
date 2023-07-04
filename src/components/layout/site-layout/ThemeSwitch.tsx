42b955d63f47a14c538440d6e4ef0410702c751f
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-03 19:16:53 -0700

    feat: modify switch to adapt to width dynamically

--- src/components/layout/site-layout/ThemeSwitch.tsx
+++ src/components/layout/site-layout/ThemeSwitch.tsx
@@ -1,26 +1,32 @@
-import { Switch } from "@/components/ui/switch";
-import { IconSwitch } from "@/components/ui/switch-icon";
-import { cn } from "@/lib/utils";
-import { MoonIcon, SunIcon } from "lucide-react";
-import { useState } from "react";
+import { Switch } from '@/components/ui/switch';
+import { IconSwitch } from '@/components/ui/switch-icon';
+import { cn } from '@/lib/utils';
+import { MoonIcon, SunIcon } from 'lucide-react';
+import { useState } from 'react';
 
 type ThemeSwitchProps = {
-  className?: string;
-}
+    className?: string;
+};
 const ThemeSwitch = (props: ThemeSwitchProps) => {
-  const [enabled, setEnabled] = useState<boolean>(false);
-  return(
-    <div className={cn('w-fit h-fit flex justify-center items-center', props.className)}>
-    <IconSwitch 
-      defaultChecked={enabled}
-      checked={enabled}
-      onCheckedChange={(checked)=>{setEnabled(checked)}}
-      
-      >
-        {enabled ? <MoonIcon /> : <SunIcon />}
-    </IconSwitch>
-    </div>
-  )
-}
+    const [enabled, setEnabled] = useState<boolean>(false);
+    return (
+        <div
+            className={cn(
+                'flex h-fit w-fit items-center justify-center',
+                props.className
+            )}
+        >
+            <IconSwitch
+                defaultChecked={enabled}
+                checked={enabled}
+                onCheckedChange={(checked) => {
+                    setEnabled(checked);
+                }}
+            >
+                {enabled ? <MoonIcon size={24} /> : <SunIcon size={24} />}
+            </IconSwitch>
+        </div>
+    );
+};
 
 export default ThemeSwitch;
