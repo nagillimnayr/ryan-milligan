1d6c9c0f3a9198a201d253581b9daa991b8fdcb6
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-03 20:01:57 -0700

    feat: make element sizing more responsive

--- src/components/hero/HeroDescription.tsx
+++ src/components/hero/HeroDescription.tsx
@@ -1,15 +1,18 @@
-import { cn } from "@/lib/utils";
-
+import { cn } from '@/lib/utils';
 
 type HeroDescProps = {
-  children: string;
-  className?: string;
-}
+    children: string;
+    className?: string;
+};
 export default function HeroDescription(props: HeroDescProps) {
-
-  return(
-    <p className={cn(props.className, 'text-primary')}>
-      {props.children}
-    </p>
-  )
+    return (
+        <p
+            className={cn(
+                props.className,
+                'overflow-hidden text-ellipsis text-center md:w-96 lg:w-96'
+            )}
+        >
+            {props.children}
+        </p>
+    );
 }
