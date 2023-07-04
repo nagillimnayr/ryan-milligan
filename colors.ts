0bff60868a3570c9f01e727cf29f6491e4e3089c
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-04 10:19:58 -0700

    feat: add colors.ts

---
+++ colors.ts
@@ -0,0 +1,24 @@
+import twColors from 'tailwindcss/colors';
+
+export const colors = {
+    violet: {
+        DEFAULT: twColors.violet[500],
+        light: twColors.violet[400],
+        dark: twColors.violet[600],
+    },
+    indigo: {
+        DEFAULT: twColors.indigo[500],
+        light: twColors.indigo[400],
+        dark: twColors.indigo[600],
+    },
+    sky: {
+        DEFAULT: twColors.sky[500],
+        light: twColors.sky[400],
+        dark: twColors.sky[600],
+    },
+    blue: {
+        DEFAULT: twColors.blue[500],
+        light: twColors.blue[400],
+        dark: twColors.blue[600],
+    },
+};
