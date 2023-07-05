e27ba242edcabea4fc70cb3844c8501ebbaff9d5
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-04 18:28:58 -0700

    build: install rehype-slug

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,15 @@
 import mdx from '@next/mdx';
 
+import remarkGfm from 'remark-gfm';
+import rehypeSlug from 'rehype-slug';
+
 const withMDX = mdx({
     extension: /\.mdx?$/,
     options: {
         // If you use remark-gfm, you'll need to use next.config.mjs
         // as the package is ESM only
-        // https://github.com/remarkjs/remark-gfm#install
-        remarkPlugins: [
-            // remarkGfm,
-            // remarkFootnotes,
-            // remarkMath
-        ],
-        rehypePlugins: [
-            // rehypeCitation,
-            // rehypeKatex,
-            // rehypeSlug,
-            // rehypeAutolinkHeadings,
-            // rehypeHighlight,
-        ],
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [rehypeSlug],
         // If you use `MDXProvider`, uncomment the following line.
         providerImportSource: '@mdx-js/react',
     },
