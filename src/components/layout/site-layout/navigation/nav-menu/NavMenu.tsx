784165fe077c1f594d0816df2d2a6ec3c905b71e
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-05 16:24:40 -0700

    feat: implement basic dropdown menu

--- src/components/layout/site-layout/navigation/nav-menu/NavMenu.tsx
+++ src/components/layout/site-layout/navigation/nav-menu/NavMenu.tsx
@@ -1,126 +1,51 @@
-import {
-    NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuIndicator,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-    NavigationMenuViewport,
-    navigationMenuTriggerStyle,
-} from '@/components/ui/navigation-menu';
-import NavMenuLink from './NavMenuLink';
-import { cn } from '@/lib/utils';
+import { Menu } from '@headlessui/react';
+import { Fragment } from 'react';
 import Link from 'next/link';
+import { cn } from '@/lib/utils';
 
-type NavItem = {
-    title: string;
-    href: string;
-    description?: string;
-};
-
-const mainNavItems: NavItem[] = [
-    {
-        title: 'Projects',
-        href: '/projects',
-    },
-    {
-        title: 'Experiments',
-        href: '/experiments',
-    },
-    {
-        title: 'Posts',
-        href: '/posts',
-    },
+const links = [
+    { href: '/account-settings', label: 'Account settings' },
+    { href: '/support', label: 'Support' },
+    { href: '/license', label: 'License' },
+    { href: '/sign-out', label: 'Sign out' },
 ];
 
-const navListItems: NavItem[] = [
-    {
-        title: 'Alert Dialog',
-        href: '/docs/primitives/alert-dialog',
-        description:
-            'A modal dialog that interrupts the user with important content and expects a response.',
-    },
-    {
-        title: 'Hover Card',
-        href: '/docs/primitives/hover-card',
-        description:
-            'For sighted users to preview content available behind a link.',
-    },
-    {
-        title: 'Progress',
-        href: '/docs/primitives/progress',
-        description:
-            'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
-    },
-    {
-        title: 'Scroll-area',
-        href: '/docs/primitives/scroll-area',
-        description: 'Visually or semantically separates content.',
-    },
-    {
-        title: 'Tabs',
-        href: '/docs/primitives/tabs',
-        description:
-            'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
-    },
-    {
-        title: 'Tooltip',
-        href: '/docs/primitives/tooltip',
-        description:
-            'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
-    },
-];
-
-type NavMenuProps = {
-    className?: string;
-};
-const NavMenu = ({ className }: NavMenuProps) => {
+const NavMenu = () => {
     return (
-        <NavigationMenu className={cn('font-display font-semibold', className)}>
-            <NavigationMenuList className="border-2 ">
-                <NavigationMenuItem>
-                    <NavigationMenuTrigger>Projects</NavigationMenuTrigger>
-                    <NavigationMenuContent className="w-full">
-                        <NavMenuLink href="/projects">Projects</NavMenuLink>
-                    </NavigationMenuContent>
-                </NavigationMenuItem>
-
-                <NavigationMenuItem>
-                    <NavigationMenuTrigger>Experiments</NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                        <NavMenuLink href="/experiments">
-                            Experiments
-                        </NavMenuLink>
-                    </NavigationMenuContent>
-                </NavigationMenuItem>
-
-                <NavigationMenuItem>
-                    <NavMenuLink href="/posts">Posts</NavMenuLink>
-                </NavigationMenuItem>
-
-                <NavigationMenuItem>
-                    <NavigationMenuTrigger>List</NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                        <ul>
-                            {navListItems.map((item) => {
+        <Menu as="div" className={'relative ml-auto flex flex-row'}>
+            <Menu.Button className={' rounded-md border-2 p-2 font-sans'}>
+                Options
+            </Menu.Button>
+            <Menu.Items
+                className={
+                    'absolute top-full flex origin-top flex-col justify-start'
+                }
+            >
+                {links.map(({ href, label }) => {
+                    return (
+                        /* Use the `active` state to conditionally style the active item.*/
+                        <Menu.Item key={href} as={Fragment}>
+                            {({ active }) => {
                                 return (
-                                    <li key={item.title}>
-                                        <NavMenuLink href={item.href}>
-                                            {item.title}
-                                        </NavMenuLink>
-                                    </li>
+                                    <Link
+                                        href={href}
+                                        className={cn(
+                                            'bg-white px-2 font-sans text-black',
+                                            {
+                                                'bg-indigo-400 text-white ':
+                                                    active,
+                                            }
+                                        )}
+                                    >
+                                        {label}
+                                    </Link>
                                 );
-                            })}
-                        </ul>
-                    </NavigationMenuContent>
-                </NavigationMenuItem>
-
-                <NavigationMenuItem>
-                    <NavMenuLink href="/contact">Contact</NavMenuLink>
-                </NavigationMenuItem>
-            </NavigationMenuList>
-        </NavigationMenu>
+                            }}
+                        </Menu.Item>
+                    );
+                })}
+            </Menu.Items>
+        </Menu>
     );
 };
 
