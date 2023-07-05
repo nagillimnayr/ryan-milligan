7f3d4da11dda37f435515698ae5f64bea0ca4c69
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-04 18:31:14 -0700

    build: import remark plugins into next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 import mdx from '@next/mdx';
 
+import remarkRehype from 'remark-rehype/lib';
 import remarkGfm from 'remark-gfm';
 import rehypeSlug from 'rehype-slug';
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkToc from 'remark-toc';
 
 const withMDX = mdx({
     extension: /\.mdx?$/,
     options: {
         // If you use remark-gfm, you'll need to use next.config.mjs
         // as the package is ESM only
-        remarkPlugins: [remarkGfm],
+        remarkPlugins: [remarkRehype, remarkGfm, remarkFrontmatter, remarkToc],
         rehypePlugins: [rehypeSlug],
         // If you use `MDXProvider`, uncomment the following line.
         providerImportSource: '@mdx-js/react',
