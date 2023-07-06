e3f062a8f4d1d7afa589c7939f3064389f08d0a0
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-06 14:56:34 -0700

    feat: add TestForm stub

---
+++ src/components/form/TestForm.tsx
@@ -0,0 +1,12 @@
+type FormProps = {
+    className?: string;
+};
+const TestForm = ({ className }: FormProps) => {
+    return (
+        <div>
+            <form></form>
+        </div>
+    );
+};
+
+export default TestForm;
