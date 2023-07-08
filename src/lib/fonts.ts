d788d398f03d9d22dc92a2377a4d6f43ffc7c059
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-07 17:46:12 -0700

    build: remove Bruno Ace font

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,212 +1,231 @@
-import { 
-  Roboto, Roboto_Flex, Roboto_Mono, Roboto_Condensed, Roboto_Slab, Roboto_Serif, Orbitron, Atkinson_Hyperlegible, Audiowide, Bruno_Ace, Bruno_Ace_SC, MuseoModerno, Iceland, Martian_Mono, Prompt, Bebas_Neue, Encode_Sans, Lexend_Peta, Exo, Exo_2, Space_Grotesk, Source_Code_Pro, Space_Mono, Trispace, Archivo_Black, Arvo, IBM_Plex_Mono, Atomic_Age, Anonymous_Pro, Koulen, Share_Tech_Mono, Syncopate, Oxanium, Archivo, Lexend
+import {
+    Roboto,
+    Roboto_Flex,
+    Roboto_Mono,
+    Roboto_Condensed,
+    Roboto_Slab,
+    Roboto_Serif,
+    Orbitron,
+    Atkinson_Hyperlegible,
+    Audiowide,
+    MuseoModerno,
+    Iceland,
+    Martian_Mono,
+    Prompt,
+    Bebas_Neue,
+    Encode_Sans,
+    Lexend_Peta,
+    Exo,
+    Exo_2,
+    Space_Grotesk,
+    Source_Code_Pro,
+    Space_Mono,
+    Trispace,
+    Archivo_Black,
+    Arvo,
+    IBM_Plex_Mono,
+    Atomic_Age,
+    Anonymous_Pro,
+    Koulen,
+    Share_Tech_Mono,
+    Syncopate,
+    Oxanium,
+    Archivo,
+    Lexend,
 } from 'next/font/google';
 
 export const roboto = Roboto({
-  subsets: ['latin'],
+    subsets: ['latin'],
     weight: '400',
     variable: '--font-roboto',
 });
 
 export const roboto_mono = Roboto_Mono({
-  subsets: ['latin'],
+    subsets: ['latin'],
     weight: '400',
     variable: '--font-roboto-mono',
 });
 
-
 export const roboto_condensed = Roboto_Condensed({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-roboto-condensed',
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-roboto-condensed',
 });
 
 export const roboto_flex = Roboto_Flex({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-roboto-flex',
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-roboto-flex',
 });
 
 export const roboto_slab = Roboto_Slab({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-roboto-slab',
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-roboto-slab',
 });
 
 export const roboto_serif = Roboto_Serif({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-roboto-serif',
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-roboto-serif',
 });
 
 export const iceland = Iceland({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-iceland',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-iceland',
+});
 
 export const orbitron = Orbitron({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-orbitron',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-orbitron',
+});
 export const atkinson_hyperlegible = Atkinson_Hyperlegible({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-atkinson-hyperlegible',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-atkinson-hyperlegible',
+});
 export const museoModerno = MuseoModerno({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-museoModerno',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-museoModerno',
+});
 
 export const prompt = Prompt({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-prompt',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-prompt',
+});
 
 export const martian_mono = Martian_Mono({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-martian-mono',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-martian-mono',
+});
 
 export const bebas_neue = Bebas_Neue({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-bebas-neue',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-bebas-neue',
+});
 
 export const encode_sans = Encode_Sans({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-encode-sans',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-encode-sans',
+});
 
 export const lexend = Lexend({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-lexend',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-lexend',
+});
 
 export const lexend_peta = Lexend_Peta({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-lexend-peta',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-lexend-peta',
+});
 
 export const exo = Exo({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-exo',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-exo',
+});
 
 export const exo_2 = Exo_2({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-exo-2',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-exo-2',
+});
 
 export const space_grotesk = Space_Grotesk({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-space-grotesk',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-space-grotesk',
+});
 
 export const space_mono = Space_Mono({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-space-mono',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-space-mono',
+});
 
 export const trispace = Trispace({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-trispace',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-trispace',
+});
 
 export const source_code_pro = Source_Code_Pro({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-source-code-pro',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-source-code-pro',
+});
 
 export const archivo = Archivo({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-archivo',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-archivo',
+});
 
 export const archivo_black = Archivo_Black({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-archivo-black',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-archivo-black',
+});
 
 export const arvo = Arvo({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-arvo',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-arvo',
+});
 
 export const ibm_plex_mono = IBM_Plex_Mono({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-ibm-plex-mono',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-ibm-plex-mono',
+});
 
 export const audiowide = Audiowide({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-audiowide',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-audiowide',
+});
 
 export const atomic_age = Atomic_Age({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-atomic-age',
-})
-
-export const bruno_ace_sc = Bruno_Ace_SC({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-bruno-ace-sc',
-})
-
-export const bruno_ace = Bruno_Ace({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-bruno-ace',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-atomic-age',
+});
 
 export const anonymous_pro = Anonymous_Pro({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-anonymous-pro',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-anonymous-pro',
+});
 
 export const koulen = Koulen({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-koulen',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-koulen',
+});
 
 export const share_tech_mono = Share_Tech_Mono({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-share-tech-mono',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-share-tech-mono',
+});
 
 export const syncopate = Syncopate({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-syncopate',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-syncopate',
+});
 
 export const oxanium = Oxanium({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-oxanium',
-})
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-oxanium',
+});
