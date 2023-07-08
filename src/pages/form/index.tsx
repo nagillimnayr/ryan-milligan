3a5f8b62803c64fe33a14e0020b040e48769352d
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-07 17:46:28 -0700

    feat: add additional test form

--- src/pages/form/index.tsx
+++ src/pages/form/index.tsx
@@ -1,5 +1,6 @@
-import TestForm from '@/components/form/TestForm';
-import TextInputForm from '@/components/form/TextInputForm';
+import FSTestForm from '@/components/forms/FSTestForm';
+import TestForm from '@/components/forms/TestForm';
+import TextInputForm from '@/components/forms/TextInputForm';
 import PageLayout from '@/components/layout/page-layout/PageLayout';
 
 const TestPage = () => {
@@ -7,18 +8,20 @@ const TestPage = () => {
         <PageLayout title="Test Form Page">
             <section
                 border="2"
-                className="flex-start container flex h-fit w-96 min-w-fit flex-col items-center gap-4 rounded-lg py-10"
+                rounded="md"
+                className="flex-start container flex h-fit w-96 min-w-fit flex-col items-center gap-4 py-10"
             >
                 <header
                     border="2"
+                    rounded="md"
                     hover="bg-gray-400/20"
                     className="flex w-full min-w-fit flex-col items-center justify-start p-2 text-center"
                 >
-                    <h2 border="2 white" p="2" m="0">
-                        Form Test
+                    <h2 border="2" p="2" m="0" rounded="md">
+                        Save To File Test
                     </h2>
                 </header>
-                <TextInputForm className="w-96 border-2 p-2" />
+                <FSTestForm className="w-96 rounded-md border-2 p-2" />
             </section>
         </PageLayout>
     );
