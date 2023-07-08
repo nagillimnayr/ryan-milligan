221bc0020662d8f79abf4356d69c14f1e389d7f7
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-07 19:04:11 -0700

    build: move experiments folder into app router

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import '~/styles/globals.css';
+import { type Metadata } from 'next';
+import SiteLayout from '@/components/layout/site-layout/SiteLayout';
+import Providers from '@/components/providers/providers';
+import type { CommonProps } from '@/components/props/Props';
+
+export const metadata: Metadata = {
+    icons: {
+        icon: '/favicon/favicon.ico',
+    },
+};
+
+// Layouts must accept a children prop.
+// This will be populated with nested layouts or pages
+export default function RootLayout({ children }: CommonProps) {
+    return (
+        <html lang="en">
+            <body suppressHydrationWarning>
+                <Providers>
+                    <SiteLayout>{children}</SiteLayout>
+                </Providers>
+            </body>
+        </html>
+    );
+}
