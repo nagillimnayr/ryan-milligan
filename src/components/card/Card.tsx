6e9b011bd5d81038d864583fd0e67d50888ca7b9
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-19 07:44:46 -0700

    feat: remove arrow

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -19,11 +19,8 @@ const Card = ({ href, heading, description, children, className }: Props) => {
             target="_blank"
             rel="noopener noreferrer"
         >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-                {heading}{' '}
-                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                    -&gt;
-                </span>
+            <h2 className={`mb-3 w-full text-center text-2xl font-semibold`}>
+                {heading}
             </h2>
             <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
                 {description}
