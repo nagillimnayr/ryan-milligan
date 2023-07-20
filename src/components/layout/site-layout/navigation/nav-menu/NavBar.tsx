2468a3a2d90d909493f0c94c5fee2f8203fee0e1
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-19 20:21:57 -0700

    feat: add color transition

--- src/components/layout/site-layout/navigation/nav-menu/NavBar.tsx
+++ src/components/layout/site-layout/navigation/nav-menu/NavBar.tsx
@@ -7,43 +7,43 @@ import NavMenuLink from './NavMenuLink';
 import { ClassNameProp } from '@/components/props/Props';
 
 const mainLinks = [
-    { href: '/projects', label: 'Projects' },
-    { href: '/experiments', label: 'Experiments' },
-    { href: '/about', label: 'About' },
-    { href: '/contact', label: 'Contact' },
+  { href: '/projects', label: 'Projects' },
+  { href: '/experiments', label: 'Experiments' },
+  { href: '/about', label: 'About' },
+  { href: '/contact', label: 'Contact' },
 ];
 
 const otherLinks = [
-    { href: '/other', label: 'Other' },
-    { href: '/misc', label: 'Misc' },
-    { href: '/etc', label: 'Etc.' },
-    { href: '/form', label: 'Form Test' },
-    { href: '/ui', label: 'UI Components' },
+  { href: '/other', label: 'Other' },
+  { href: '/misc', label: 'Misc' },
+  { href: '/etc', label: 'Etc.' },
+  { href: '/form', label: 'Form Test' },
+  { href: '/ui', label: 'UI Components' },
 ];
 
-const btnStyle = `rounded-md border-0 py-2 px-4 font-sans h-full hover:bg-foreground/10 ui-open:bg-foreground/10`;
+const btnStyle = `rounded-md transition-colors duration-300 border-0 py-2 px-4 font-sans h-full hover:bg-foreground/10 ui-open:bg-foreground/10`;
 
 const Navbar = ({ className }: ClassNameProp) => {
-    return (
-        <nav
-            className={cn(
-                'relative ml-auto flex h-fit w-fit flex-row items-center justify-start gap-0 rounded-md border-2 p-1',
-                className
-            )}
-        >
-            {mainLinks.map(({ href, label }) => {
-                return (
-                    <NavMenuLink key={href} href={href} className={btnStyle}>
-                        {label}
-                    </NavMenuLink>
-                );
-            })}
+  return (
+    <nav
+      className={cn(
+        'relative ml-auto flex h-fit w-fit flex-row items-center justify-start gap-0 rounded-md border-2 p-1',
+        className
+      )}
+    >
+      {mainLinks.map(({ href, label }) => {
+        return (
+          <NavMenuLink key={href} href={href} className={btnStyle}>
+            {label}
+          </NavMenuLink>
+        );
+      })}
 
-            <NavDropdownMenu links={otherLinks} className={btnStyle}>
-                <div className="inline-flex gap-3">Other</div>
-            </NavDropdownMenu>
-        </nav>
-    );
+      {/* <NavDropdownMenu links={otherLinks} className={btnStyle}>
+        <div className="inline-flex gap-3">Other</div>
+      </NavDropdownMenu> */}
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/components/layout/site-layout/navigation/nav-menu/NavDropdownMenu.tsx
+++ src/components/layout/site-layout/navigation/nav-menu/NavDropdownMenu.tsx
@@ -6,57 +6,55 @@ import { ChevronDownIcon } from 'lucide-react';
 import type { CommonProps } from '@/components/props/Props';
 
 type link = {
-    href: string;
-    label: string;
+  href: string;
+  label: string;
 };
 
 type NavDropdownProps = CommonProps & {
-    links: link[];
+  links: link[];
 };
 const NavDropdownMenu = ({ children, className, links }: NavDropdownProps) => {
-    return (
-        <Menu as="div" className={'relative ml-auto flex flex-col'}>
-            <div className="relative">
-                <Menu.Button className={cn(className)}>
-                    {({ open }) => {
-                        // render prop to switch chevron direction
-                        return (
-                            <div className="inline-flex items-center justify-start gap-3">
-                                {children}{' '}
-                                <ChevronDownIcon
-                                    data-open={open ? 'open' : 'closed'}
-                                    className={
-                                        'transition-transform ui-open:-rotate-180'
-                                    }
-                                />
-                            </div>
-                        );
-                    }}
-                </Menu.Button>
-                <Menu.Items
-                    className={
-                        'dropdown-center flex w-fit min-w-fit  flex-col justify-start whitespace-nowrap rounded-md border-2 bg-popover text-popover-foreground'
-                    }
+  return (
+    <Menu as="div" className={'relative ml-auto flex flex-col'}>
+      <div className="relative">
+        <Menu.Button className={cn(className)}>
+          {({ open }) => {
+            // render prop to switch chevron direction
+            return (
+              <div className="inline-flex items-center justify-start gap-3">
+                {children}{' '}
+                <ChevronDownIcon
+                  data-open={open ? 'open' : 'closed'}
+                  className={'transition-transform ui-open:-rotate-180'}
+                />
+              </div>
+            );
+          }}
+        </Menu.Button>
+        <Menu.Items
+          className={
+            'dropdown-center flex w-fit min-w-fit  flex-col justify-start whitespace-nowrap rounded-md border-2 bg-popover text-popover-foreground'
+          }
+        >
+          {links.map(({ href, label }) => {
+            return (
+              /* Use the `active` state to conditionally style the active item.*/
+              <Menu.Item key={href} as={Fragment}>
+                <Link
+                  href={href}
+                  className={
+                    'min-w-full rounded-md bg-transparent px-2 py-1 font-sans  ui-active:bg-indigo-400 ui-active:text-white'
+                  }
                 >
-                    {links.map(({ href, label }) => {
-                        return (
-                            /* Use the `active` state to conditionally style the active item.*/
-                            <Menu.Item key={href} as={Fragment}>
-                                <Link
-                                    href={href}
-                                    className={
-                                        'min-w-full rounded-md bg-transparent px-2 py-1 font-sans  ui-active:bg-indigo-400 ui-active:text-white'
-                                    }
-                                >
-                                    {label}
-                                </Link>
-                            </Menu.Item>
-                        );
-                    })}
-                </Menu.Items>
-            </div>
-        </Menu>
-    );
+                  {label}
+                </Link>
+              </Menu.Item>
+            );
+          })}
+        </Menu.Items>
+      </div>
+    </Menu>
+  );
 };
 
 export default NavDropdownMenu;
