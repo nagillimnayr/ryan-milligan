406e6b2c728ffaa97d5c0d751ec3d67211732b54
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-19 19:09:58 -0700

    feat: set duration

--- src/components/ContactLinks.tsx
+++ src/components/ContactLinks.tsx
@@ -6,16 +6,16 @@ const ContactLinks = () => {
       {/** Github */}
       <Link
         href="https://github.com/nagillimnayr"
-        className="aspect-square h-full overflow-hidden rounded-full transition-transform duration-300  hover:scale-110"
+        className="aspect-square h-full overflow-hidden rounded-full transition-transform duration-300 hover:scale-110"
       >
-        <span className="icon-[mdi--github] h-full w-full rounded-full transition-all duration-300  hover:text-blue-500" />
+        <span className="icon-[mdi--github] h-full w-full rounded-full transition-all duration-300 hover:text-blue-500" />
       </Link>
       {/** LinkedIn */}
       <Link
         href="https://www.linkedin.com/in/ryan-milligan-837352205/"
         className="aspect-square h-full overflow-hidden rounded-full transition-transform duration-300 hover:scale-110 "
       >
-        <span className="icon-[mdi--linkedin] h-full w-full rounded-full transition-all duration-300  hover:text-blue-500" />
+        <span className="icon-[mdi--linkedin] h-full w-full rounded-full transition-all duration-300 hover:text-blue-500" />
       </Link>
     </div>
   );
--- src/components/layout/site-layout/SiteHeader.tsx
+++ src/components/layout/site-layout/SiteHeader.tsx
@@ -3,25 +3,25 @@ import ThemeSwitch from './ThemeSwitch';
 import Navbar from './navigation/nav-menu/NavBar';
 
 export default function SiteHeader() {
-    return (
-        <header className="sticky top-0 z-50 flex h-20 flex-row items-center justify-start border-b-2 border-b-border bg-background px-8">
-            <Link
-                href={'/'}
-                className="flex h-fit w-fit flex-row items-center justify-start gap-4"
-            >
-                <div className="inline-flex aspect-square w-10 items-center justify-center">
-                    <div className="icon-[eos-icons--atom-electron]  h-full w-full rounded-full border-2 transition-colors hover:text-indigo-500" />
-                </div>
-                <h2 className="font-orbitron text-3xl font-extrabold">
-                    <span className="bg-gradient-to-r from-indigo-500 to-violet-500 bg-clip-text transition-colors hover:text-transparent">
-                        Ryan Milligan
-                    </span>
-                </h2>
-            </Link>
+  return (
+    <header className="sticky top-0 z-50 flex h-20 flex-row items-center justify-start border-b-2 border-b-border bg-background px-8">
+      <Link
+        href={'/'}
+        className="flex h-fit w-fit flex-row items-center justify-start gap-4"
+      >
+        <div className="inline-flex aspect-square w-10 items-center justify-center">
+          <div className="icon-[eos-icons--atom-electron]  h-full w-full rounded-full border-2 transition-colors duration-300 hover:text-indigo-500" />
+        </div>
+        <h2 className="font-orbitron text-3xl font-extrabold">
+          <span className="bg-gradient-to-r from-indigo-500 to-violet-500 bg-clip-text transition-colors duration-300 hover:text-transparent">
+            Ryan Milligan
+          </span>
+        </h2>
+      </Link>
 
-            {/* <MainNav /> */}
-            <Navbar />
-            <ThemeSwitch className="ml-auto mr-4" />
-        </header>
-    );
+      {/* <MainNav /> */}
+      <Navbar />
+      <ThemeSwitch className="ml-auto mr-4" />
+    </header>
+  );
 }
