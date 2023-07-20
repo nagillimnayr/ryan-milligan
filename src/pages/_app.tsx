c8ce9fba40da62e32deac72b9fdd1660af3a96b2
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-19 20:18:25 -0700

    feat: setup trpc router

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
 import SiteLayout from '@/components/layout/site-layout/SiteLayout';
 import Providers from '@/components/providers/providers';
+import { trpc } from '@/lib/trpc/trpc';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import type { AppType } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <Providers>
-            <SiteLayout>
-                <Component {...pageProps} />
-            </SiteLayout>
-        </Providers>
-    );
-}
+const App: AppType = ({ Component, pageProps }: AppProps) => {
+  return (
+    <Providers>
+      <SiteLayout>
+        <Component {...pageProps} />
+      </SiteLayout>
+    </Providers>
+  );
+};
+
+export default trpc.withTRPC(App);
