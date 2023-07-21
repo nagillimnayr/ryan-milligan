c9c9698020858c01710898ae2209ea275ff0d4ac
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-21 11:26:34 -0700

    feat: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,19 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
     return (
         <Html lang="en" >
-            <Head />
+            <Head >
+                
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/favicon/favicon.svg"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon/favicon.png"
+        />
+            </Head>
             <body >
                 <Main />
                 <NextScript />
