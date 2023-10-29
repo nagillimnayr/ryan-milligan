3b59def19ec72ce4fec89ea772a4a80b1bdd932c
Ryan Milligan <ryanmilligan88@gmail.com>
2023-10-29 10:53:59 -0700

    add cameraPosition prop

--- src/components/canvas/scene/View.tsx
+++ src/components/canvas/scene/View.tsx
@@ -14,19 +14,22 @@ import {
   CameraControls,
 } from '@react-three/drei';
 import { Three } from '@/helpers/tunnel/Three';
-import { type ColorRepresentation } from 'three';
+import { type Vector3Tuple, type ColorRepresentation } from 'three';
 
 type CommonProps = {
   color?: ColorRepresentation;
+  cameraPosition?: Vector3Tuple;
 };
-export const Common = ({ color }: CommonProps) => (
+export const Common = ({ color,
+  cameraPosition = [0,0,10]
+}: CommonProps) => (
   <Suspense fallback={null}>
     {color && <color attach='background' args={[color]} />}
     <ambientLight intensity={0.5} />
     <pointLight position={[20, 30, 10]} intensity={1} />
-    <pointLight position={[-10, -10, -10]} color='blue' />
+    {/* <pointLight position={[-10, -10, -10]} color='blue' /> */}
     <directionalLight position-z={1} intensity={0.1} />
-    <PerspectiveCamera makeDefault fov={40} position={[0, 0, 1]} />
+    <PerspectiveCamera makeDefault fov={40} position={cameraPosition} />
   </Suspense>
 );
 
