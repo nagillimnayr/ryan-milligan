9d5ab75e28e485f6c6cd5d326a4c84c58b9995e4
Ryan Milligan <ryanmilligan88@gmail.com>
2023-10-29 10:53:47 -0700

    add Name3D

---
+++ src/components/canvas/Name3D.tsx
@@ -0,0 +1,16 @@
+import { Center, Text3D } from '@react-three/drei';
+
+export const Name3D = function Name3D() {
+
+
+  const fontURL = 'fonts/Roboto_Regular.json';
+  return(
+    <>
+    <Center>
+
+      <Text3D
+      font={fontURL} letterSpacing={0.15}>Ryan Milligan</Text3D>
+      </Center>
+    </>
+  )
+}
