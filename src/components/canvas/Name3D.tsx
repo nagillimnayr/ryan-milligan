0a849bacb7482939870038f2c22c418a089907c3
Ryan Milligan <ryanmilligan88@gmail.com>
2023-10-30 20:58:31 -0700

    change text color based on theme

--- src/components/canvas/Name3D.tsx
+++ src/components/canvas/Name3D.tsx
@@ -1,12 +1,15 @@
 import { Center, Text3D } from '@react-three/drei';
+import { useTheme } from 'next-themes';
 
 const FONT_URL = 'fonts/Roboto_Regular.json';
 
 export const Name3D = function Name3D() {
+  const { theme } = useTheme();
+  const color = theme === 'dark' ? 'white' : 'black';
   return (
     <>
       <Center>
-        <Text3D font={FONT_URL} letterSpacing={0.05}>
+        <Text3D font={FONT_URL} letterSpacing={0.05} material-color={color}>
           Ryan Milligan
         </Text3D>
       </Center>
