070d179fb6ed886f01641df802c056fda95ddccb
Ryan Milligan <ryanmilligan88@gmail.com>
2023-11-07 13:23:08 -0800

    add directional light helper

--- src/components/canvas/scene/MainScene.tsx
+++ src/components/canvas/scene/MainScene.tsx
@@ -1,11 +1,19 @@
-import { Grid, PerspectiveCamera, Sphere } from '@react-three/drei';
+import { Grid, PerspectiveCamera, Sphere, useHelper } from '@react-three/drei';
 import { Name3D } from '../Name3D';
+import { useRef } from 'react';
+import { DirectionalLight, DirectionalLightHelper } from 'three';
 
 export const MainScene = () => {
+  const dirLightRef = useRef<DirectionalLight>(null!);
+  const dirLightHelper = useHelper(dirLightRef, DirectionalLightHelper);
   return (
     <>
       <PerspectiveCamera makeDefault position-z={3} position-y={3} />
-      <directionalLight position={[0, 30, 20]} intensity={2.0} />
+      <directionalLight
+        ref={dirLightRef}
+        position={[0, 30, 20]}
+        intensity={2.0}
+      />
       <Name3D position={[0, 0, -5]} scale={[1, 1, 0.5]} />
       <Grid infiniteGrid />
     </>
