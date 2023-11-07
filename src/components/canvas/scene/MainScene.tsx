732546069e113cf2f1172607521aba2f79bf1658
Ryan Milligan <ryanmilligan88@gmail.com>
2023-11-07 13:52:32 -0800

    add slight ambient light

--- src/components/canvas/scene/MainScene.tsx
+++ src/components/canvas/scene/MainScene.tsx
@@ -14,8 +14,13 @@ export const MainScene = () => {
         position={[0, 30, 20]}
         intensity={2.0}
       />
+      <ambientLight intensity={0.2} color={'#3a74f7'} />
       <Name3D position={[0, 0, -5]} scale={[1, 1, 0.5]} />
       <Grid infiniteGrid />
+
+      <Sphere args={[1, 128, 128]} position-y={1}>
+        <meshStandardMaterial color={'red'} metalness={0.5} />
+      </Sphere>
     </>
   );
 };
