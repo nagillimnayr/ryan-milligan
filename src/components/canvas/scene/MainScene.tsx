bfb19ccdb563d2bba6c51f9e6941444a43d089a7
Ryan Milligan <ryanmilligan88@gmail.com>
2023-11-07 13:04:39 -0800

    remove sphere

--- src/components/canvas/scene/MainScene.tsx
+++ src/components/canvas/scene/MainScene.tsx
@@ -4,10 +4,9 @@ import { Name3D } from '../Name3D';
 export const MainScene = () => {
   return (
     <>
-      <PerspectiveCamera makeDefault position-z={3} />
+      <PerspectiveCamera makeDefault position-z={3} position-y={3} />
       <directionalLight position={[0, 30, 20]} intensity={2.0} />
-      <Sphere args={[1]} position-y={1} />
-      <Name3D position={[0, 0, 0]} scale={[1, 1, 0.5]} />
+      <Name3D position={[0, 0, -5]} scale={[1, 1, 0.5]} />
       <Grid infiniteGrid />
     </>
   );
