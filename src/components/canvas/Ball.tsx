44e3cbb1fdc62922b3bbb2167aa48d648fa952bd
Ryan Milligan <ryanmilligan88@gmail.com>
2023-11-07 21:34:21 -0800

    make color optional

--- src/components/canvas/Ball.tsx
+++ src/components/canvas/Ball.tsx
@@ -3,9 +3,9 @@ import { RigidBody } from '@react-three/rapier';
 import { type ColorRepresentation, type SphereGeometry } from 'three';
 
 export type BallProps = ShapeProps<typeof SphereGeometry> & {
-  color: ColorRepresentation;
+  color?: ColorRepresentation;
 };
-export const Ball = ({ children, color, ...props }: BallProps) => {
+export const Ball = ({ children, color = 'red', ...props }: BallProps) => {
   return (
     <RigidBody>
       <Sphere castShadow receiveShadow {...props}>
