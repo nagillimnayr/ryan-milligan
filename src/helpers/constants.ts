28084d68ecb027af6ca97649d02a8c0cd65ad488
Ryan Milligan <ryanmilligan88@gmail.com>
2023-11-07 16:04:44 -0800

    add radian constants

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -3,3 +3,9 @@ import { Vector3 } from 'three';
 export const X_AXIS = new Vector3(0, 0, 1);
 export const Y_AXIS = new Vector3(0, 0, 1);
 export const Z_AXIS = new Vector3(0, 0, 1);
+
+export const PI = Math.PI;
+export const TWO_PI = PI * 2;
+export const PI_OVER_TWO = PI / 2;
+export const PI_OVER_THREE = PI / 3;
+export const PI_OVER_FOUR = PI / 4;
