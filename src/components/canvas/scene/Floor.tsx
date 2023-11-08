a3a4ccd303659afc2067a53da0188ccc7a6e15f6
Ryan Milligan <ryanmilligan88@gmail.com>
2023-11-07 16:04:53 -0800

    add Floor

---
+++ src/components/canvas/scene/Floor.tsx
@@ -0,0 +1,22 @@
+import { Grid, Plane } from '@react-three/drei';
+import { useMemo } from 'react';
+import { PI_OVER_TWO } from '../../../helpers/constants';
+import { FrontSide } from 'three';
+
+export const Floor = () => {
+  const args: [number, number, number, number] = useMemo(() => {
+    const width = 20;
+    const height = 20;
+    const widthSegments = 20;
+    const heightSegments = 20;
+    return [width, height, widthSegments, heightSegments];
+  }, []);
+  return (
+    <group>
+      <Grid args={args} position-y={0.01} />
+      <Plane rotation-x={-PI_OVER_TWO} args={args} receiveShadow>
+        <meshStandardMaterial shadowSide={FrontSide} />
+      </Plane>
+    </group>
+  );
+};
