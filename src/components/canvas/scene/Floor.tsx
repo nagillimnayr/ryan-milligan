2fdf849a41af8fc63073ac480ea6028415127939
Ryan Milligan <ryanmilligan88@gmail.com>
2023-11-07 21:32:01 -0800

    wrap in RigidBody

--- src/components/canvas/scene/Floor.tsx
+++ src/components/canvas/scene/Floor.tsx
@@ -1,7 +1,8 @@
 import { Grid, Plane } from '@react-three/drei';
 import { useMemo } from 'react';
-import { PI_OVER_TWO } from '../../../helpers/constants';
+import { PI_OVER_TWO } from '@/helpers/constants';
 import { FrontSide } from 'three';
+import { RigidBody } from '@react-three/rapier';
 
 export const Floor = () => {
   const args: [number, number, number, number] = useMemo(() => {
@@ -14,9 +15,11 @@ export const Floor = () => {
   return (
     <group>
       <Grid args={args} position-y={0.01} />
-      <Plane rotation-x={-PI_OVER_TWO} args={args} receiveShadow>
-        <meshStandardMaterial shadowSide={FrontSide} />
-      </Plane>
+      <RigidBody>
+        <Plane rotation-x={-PI_OVER_TWO} args={args} receiveShadow>
+          <meshStandardMaterial shadowSide={FrontSide} />
+        </Plane>
+      </RigidBody>
     </group>
   );
 };
