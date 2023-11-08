fb3387fd55b5e58bb4630f2d764b327748a16b50
Ryan Milligan <ryanmilligan88@gmail.com>
2023-11-07 21:31:50 -0800

    add Physics

--- src/components/canvas/scene/Scene.tsx
+++ src/components/canvas/scene/Scene.tsx
@@ -3,15 +3,21 @@
 import { Canvas, CanvasProps } from '@react-three/fiber';
 import { Preload, CameraControls, PerspectiveCamera } from '@react-three/drei';
 import { r3f } from '@/helpers/tunnel/tunnel';
+import { Suspense } from 'react';
+import { Physics } from '@react-three/rapier';
 
 type SceneProps = Omit<CanvasProps, 'children'>;
 export default function Scene({ ...props }: SceneProps) {
   // Everything defined in here will persist between route changes, only children are swapped
   return (
     <Canvas {...props} shadows>
-      {/* @ts-ignore */}
-      <r3f.Out />
-      <Preload all />
+      <Suspense>
+        <Physics>
+          {/* @ts-ignore */}
+          <r3f.Out />
+          <Preload all />
+        </Physics>
+      </Suspense>
     </Canvas>
   );
 }
