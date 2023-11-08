5580d6291200fc691aad14192096c88ba98e13be
Ryan Milligan <ryanmilligan88@gmail.com>
2023-11-07 16:40:22 -0800

    add PointLight

--- src/components/canvas/scene/MainScene.tsx
+++ src/components/canvas/scene/MainScene.tsx
@@ -1,23 +1,41 @@
 import { Grid, PerspectiveCamera, Sphere, useHelper } from '@react-three/drei';
 import { Name3D } from '../Name/Name3D';
 import { useRef } from 'react';
-import { DirectionalLight, DirectionalLightHelper, FrontSide } from 'three';
+import {
+  DirectionalLight,
+  DirectionalLightHelper,
+  FrontSide,
+  PointLight,
+  PointLightHelper,
+} from 'three';
 import { Floor } from './Floor';
+import { degToRad } from 'three/src/math/MathUtils';
 
 export const MainScene = () => {
-  const dirLightRef = useRef<DirectionalLight>(null!);
-  const dirLightHelper = useHelper(dirLightRef, DirectionalLightHelper);
+  // const dirLightRef = useRef<DirectionalLight>(null!);
+  const pointLightRef = useRef<PointLight>(null!);
+  // useHelper(dirLightRef, DirectionalLightHelper);
+  useHelper(pointLightRef, PointLightHelper);
   return (
     <>
       <PerspectiveCamera makeDefault position-z={3} position-y={3} />
-      <directionalLight
+      {/* <directionalLight
         ref={dirLightRef}
-        position={[0, 30, 20]}
+        position={[10, 1, 20]}
         intensity={2.0}
         castShadow
+      /> */}
+
+      <pointLight
+        ref={pointLightRef}
+        position={[0, 5, 0]}
+        castShadow
+        distance={100}
+        intensity={20}
       />
+
       <ambientLight intensity={0.2} color={'#3a74f7'} />
-      <Name3D position={[0, 0, -5]} scale={[1, 1, 0.5]}>
+      <Name3D position={[-5, 0, -5]} rotation-y={degToRad(45)}>
         RYAN
       </Name3D>
       <Floor />
