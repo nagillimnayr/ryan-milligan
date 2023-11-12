e0610981c579e5c4516afe0a75a176b5f7b2b3d1
Ryan Milligan <ryanmilligan88@gmail.com>
2023-11-12 09:37:49 -0800

    add transmission material

--- src/components/canvas/Ball.tsx
+++ src/components/canvas/Ball.tsx
@@ -1,4 +1,8 @@
-import { Sphere, type ShapeProps } from '@react-three/drei';
+import {
+  Sphere,
+  type ShapeProps,
+  MeshTransmissionMaterial,
+} from '@react-three/drei';
 import { BallCollider, RigidBody, BallArgs } from '@react-three/rapier';
 import { type ColorRepresentation, type SphereGeometry } from 'three';
 import { Select } from '@react-three/postprocessing';
@@ -17,14 +21,19 @@ export const Ball = ({ children, color = 'red', ...props }: BallProps) => {
   }, []);
   return (
     <>
-      <RigidBody>
+      <RigidBody colliders={'ball'}>
         <Select enabled={true}>
           <Sphere castShadow receiveShadow {...props} onClick={handleClick}>
             {children}
-            <meshStandardMaterial color={color} />
+            {/* <meshStandardMaterial color={color} /> */}
+            <MeshTransmissionMaterial
+              color={color}
+              distortionScale={0.5}
+              temporalDistortion={0.0}
+            />
           </Sphere>
         </Select>
-        <BallCollider args={[1]} />
+        {/* <BallCollider args={[1]} /> */}
       </RigidBody>
     </>
   );
