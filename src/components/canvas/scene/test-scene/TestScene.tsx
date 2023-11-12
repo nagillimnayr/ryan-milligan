a6b3c74288b77cf9ffc1c076af60b2ecae0c8d7b
Ryan Milligan <ryanmilligan88@gmail.com>
2023-11-12 10:41:23 -0800

    add light to TestScene

--- src/components/canvas/scene/test-scene/TestScene.tsx
+++ src/components/canvas/scene/test-scene/TestScene.tsx
@@ -1,13 +1,28 @@
+import { useRef } from 'react';
 import { CameraManager } from '../CameraManager';
 import { Floor } from '../Floor';
+import * as THREE from 'three';
+import { useHelper } from '@react-three/drei';
 
 export const TestScene = () => {
+  const pointLightRef = useRef<THREE.PointLight>(null!);
+
+  useHelper(pointLightRef, THREE.PointLightHelper, 1, 'red');
+
   return (
     <>
       <color attach={'background'} args={['#202020']} />
       {/* <fog attach={'fog'} args={['#202020', 5, 20]} /> */}
       <CameraManager />
       <Floor />
+
+      <pointLight
+        ref={pointLightRef}
+        castShadow
+        position-y={10}
+        distance={100}
+        intensity={100}
+      />
     </>
   );
 };
