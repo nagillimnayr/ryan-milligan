8d9946f3c6528d0ca2ad32c87f9a362e6abe90dd
Ryan Milligan <ryanmilligan88@gmail.com>
2023-11-14 22:54:11 -0800

    use import alias

--- src/components/canvas/scene/test-scene/TestScene.tsx
+++ src/components/canvas/scene/test-scene/TestScene.tsx
@@ -7,7 +7,7 @@ import { TrackingSpotLight } from '../lighting/TrackingSpotLight';
 import { Cube } from './Cube';
 import { TWO_PI } from '@/helpers/constants';
 import { generateUUID } from 'three/src/math/MathUtils';
-import { Brick } from '../../brick/Brick';
+import { Brick } from '@/components/canvas/brick/Brick';
 
 const RAND_POS_SCALAR = 20;
 const RAND_SCALE_SCALAR = 1;
@@ -25,13 +25,32 @@ export const TestScene = () => {
       <CameraManager />
       <Floor />
 
-      {/* <pointLight
+      <pointLight
         ref={pointLightRef}
         castShadow
         position-y={10}
+        position-z={5}
         distance={100}
         intensity={100}
-      /> */}
+      />
+      <pointLight
+        ref={pointLightRef}
+        castShadow
+        position-y={10}
+        position-z={-5}
+        position-x={-5}
+        distance={100}
+        intensity={100}
+      />
+      <pointLight
+        ref={pointLightRef}
+        castShadow
+        position-y={1}
+        position-z={0}
+        position-x={-2}
+        distance={100}
+        intensity={10}
+      />
 
       <TrackingSpotLight position={[2, 20, 5]} />
 
