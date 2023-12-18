1b1845a2d1cda028c79fd06da2b3cb2b868ed6c6
Ryan Milligan <ryanmilligan88@gmail.com>
2023-12-18 14:43:04 -0800

    add input actions

--- src/state/controller-machine.ts
+++ src/state/controller-machine.ts
@@ -1,10 +1,13 @@
-import { type RootState } from '@react-three/fiber';
-import { EventDispatcher } from 'three';
+import { type RootState as ThreeState } from '@react-three/fiber';
+import { RapierRigidBody } from '@react-three/rapier';
+import { type EventDispatcher } from 'three';
 import { assign, createMachine } from 'xstate';
 
 type ControllerContext = {
-  getThree: () => RootState;
+  getThree: () => ThreeState;
   eventManager: EventDispatcher<any>;
+
+  player: RapierRigidBody;
 };
 
 type ControllerEvents =
@@ -27,9 +30,36 @@ export const ControllerMachine = createMachine(
     context: () => ({
       getThree: null!,
       eventManager: null!,
+      player: null!,
     }),
+
+    initial: 'idle',
+    states: {
+      idle: {
+        on: {
+          INPUT_FORWARD: { actions: ['inputForward'] },
+          INPUT_BACKWARD: { actions: ['inputBackward'] },
+          INPUT_LEFT: { actions: ['inputLeft'] },
+          INPUT_RIGHT: { actions: ['inputRight'] },
+        },
+      },
+    },
   },
   {
-    actions: {},
+    actions: {
+      inputForward(context, event, meta) {
+        const { player } = context;
+        // player.applyImpulse();
+      },
+      inputBackward(context, event, meta) {
+        const { player } = context;
+      },
+      inputLeft(context, event, meta) {
+        const { player } = context;
+      },
+      inputRight(context, event, meta) {
+        const { player } = context;
+      },
+    },
   },
 );
